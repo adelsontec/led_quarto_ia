e8e08b2a06a59acb5dbf9b6b03518b0ea015c197
adelsontec <52478253+adelsontec@users.noreply.github.com>
2025-04-13 06:01:43 -0300

    Update esp32_led_wifi.ino

--- esp32_led_wifi.ino
+++ esp32_led_wifi.ino
@@ -1,1 +1,54 @@
+#include <WiFi.h>
 
+const char* ssid = "SUA_REDE_WIFI";
+const char* password = "SUA_SENHA";
+
+WiFiServer server(3232);
+const int ledPin = 2;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado!");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  
+  if (client) {
+    String comando = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          comando.trim();
+          if (comando == "ligar") {
+            digitalWrite(ledPin, HIGH);
+            client.println("LED LIGADO");
+          } else if (comando == "desligar") {
+            digitalWrite(ledPin, LOW);
+            client.println("LED DESLIGADO");
+          } else {
+            client.println("Comando inválido");
+          }
+          comando = "";
+        } else {
+          comando += c;
+        }
+      }
+    }
+    client.stop();
+  }
+}
